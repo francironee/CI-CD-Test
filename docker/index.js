17225feb76dd43d688f2205eaea45b872708cbef
francironee <francirone03@gmail.com>
2022-07-07 16:06:29 -0300

    a

--- docker/index.js
+++ docker/index.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-const gitHash = process.env.GITHASH
-const date = process.env.DATE
+const gitHash = process.env.GIT_COMMIT
+const date = process.env.TIME
 
 app.get('/', (req, res) => {
   res.send("Hello world CIRONE y MATRERO ... V. " + date + gitHash)
